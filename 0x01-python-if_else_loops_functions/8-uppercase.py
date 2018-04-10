1a7ce0e8129d42313a3d7897bb471672138a04cc
IOLevi <337@holbertonschool.com>
2018-04-10 14:50:06 -0700

    pep8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-def islower(i):
-    """determines of char is lowercase"""
-    if ord(i) >= 97 and ord(i) < 123:
-        return True
-    else:
-        return False
-
-
 def uppercase(str):
     """prints a string in uppercase"""
     for i in str:
-        print("{}".format(chr(ord(i) - 32) if islower(i) else i), end='')
+        print("{}".format(chr(ord(i) - 32) if
+                          ord(i) >= 97 and ord(i) < 123 else i), end='')
     print()
