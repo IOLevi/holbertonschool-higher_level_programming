3c23ac4e213198a75817e840fdaa4c295dd1d5d5
IOLevi <337@holbertonschool.com>
2018-04-12 13:49:34 -0700

    one line version

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) > 1:
-        a = sum((int(j) for i in range(1, len(sys.argv)) for j in sys.argv[i]))
-        print(a)
+       print(sum((int(j) for i in range(1, len(sys.argv)) for j in sys.argv[i])))
+        
