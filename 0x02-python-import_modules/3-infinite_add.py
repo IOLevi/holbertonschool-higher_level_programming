7f28e3555d1311181dde7143f9f1be8f603089fa
IOLevi <337@holbertonschool.com>
2018-04-12 13:47:36 -0700

    account for no arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    a = sum((int(j) for i in range(1, len(sys.argv)) for j in sys.argv[i]))
-    print(a)
+    if len(sys.argv) > 1:
+        a = sum((int(j) for i in range(1, len(sys.argv)) for j in sys.argv[i]))
+        print(a)
