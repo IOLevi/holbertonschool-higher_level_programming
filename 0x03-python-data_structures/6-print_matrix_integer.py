afb77c924a5ab5ac98b95a9f4724708eec303b82
IOLevi <337@holbertonschool.com>
2018-04-17 16:11:21 -0700

    forgot many colons

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,9 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for i, d in enumerate(row):
-            if i == len(row) - 1
+            if i == len(row) - 1:
                 print("{:d}".format(d))
             else:
                 print("{:d}".format(d), end=' ')
-    if matrix == [[]]
+    if matrix == [[]]:
         print()
