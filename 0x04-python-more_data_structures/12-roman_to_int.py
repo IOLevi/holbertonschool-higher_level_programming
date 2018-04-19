de08c7a7e99dd0b048f462e301a1ca1eb62a7b31
IOLevi <337@holbertonschool.com>
2018-04-19 15:14:12 -0700

    first try

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    a = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
+    b = dict(IV=4, IX=9, XL=40, XC=90, CD=400, CM=900)
+    num = 0
+    if not isinstance(roman_string, str):
+        return 0
+
+    rs = roman_string
+
+    for i in sorted(b.keys()):
+        if i in rs:
+            num += b[i]
+            rs = rs.replace(i, "")
+
+    for i in rs:
+        num += a[i]
+    return num
