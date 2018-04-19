43dda0de022bb8c8772f44c34374622c669381a1
IOLevi <337@holbertonschool.com>
2018-04-19 12:50:33 -0700

    first commit

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    a = []
+    winner = None
+    for k, v in iter(a_dictionary):
+        a.append = v
+        if max(a) == v
+            winner = k
+
+    return winner
---
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+def only_diff_elments(set_1, set_2):
+    return set_1 ^ set_2
