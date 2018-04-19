fe30c76581b0b662cbda0cb3a3689e26313f522f
IOLevi <337@holbertonschool.com>
2018-04-19 13:07:51 -0700

    added a return

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    list(map(lambda x: x if x is not search else replace, my_list))
+    return list(map(lambda x: x if x is not search else replace, my_list))
