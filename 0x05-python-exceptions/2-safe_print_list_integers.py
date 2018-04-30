78769b0432282a406ff432922e94f693fd25b658
IOLevi <337@holbertonschool.com>
2018-04-30 15:39:18 -0700

    logic change

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,9 +4,9 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for i in range(x):
         try:
-            print("{:d}".format(mylist[i]), end="")
+            print("{:d}".format(my_list[i]), end="")
             printed += 1
-        except (ValueError, IndexError):
+        except (TypeError, ValueError):
             pass
     print()
     return printed
