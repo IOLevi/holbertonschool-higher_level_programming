1c7f3d257ec2bcd01ced51fc88c128aa680c2e9b
IOLevi <35237068+IOLevi@users.noreply.github.com>
2018-05-01 17:25:08 -0700

    Delete 1-square.py

--- 1-square.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Class that defines a square"""
-
-class Square():
-    """square class"""
-    def __init__(self, size):
-        """instantiation"""
-        self.__size = size
-    
