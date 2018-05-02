3f19d2ed960a883ac50317ac05a802cf3c1446ad
IOLevi <337@holbertonschool.com>
2018-05-01 17:24:10 -0700

    move to right folder

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class that defines a square"""
+
+class Square():
+    """square class"""
+    def __init__(self, size):
+        """instantiation"""
+        self.__size = size
+    
