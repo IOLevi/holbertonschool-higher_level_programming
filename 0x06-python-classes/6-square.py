89f934d5c581d2d96ec10077286f334341e80489
IOLevi <337@holbertonschool.com>
2018-05-01 19:53:53 -0700

    logic improvements

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -18,7 +18,10 @@ class Square():
         if self.__size == 0:
             print()
         else:
+            for k in range(self.__position[1]):
+                print()
             for i in range(self.__size):
+                print(" " * self.__position[0], end="")
                 for j in range(self.__size):
                     print("#", end="")
                 print()
