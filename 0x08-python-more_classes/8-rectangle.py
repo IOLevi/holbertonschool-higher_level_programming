07495d31c5b4eaeb9b7bd46775191d0ace3dc832
IOLevi <337@holbertonschool.com>
2018-05-09 11:13:23 -0700

    changed return type of static

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -80,6 +80,6 @@ class Rectangle():
         if not isinstance(rect_2, Rectangle):
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_2.area > rect_1.area:
-            return False
+            return rect_2 
         else:
-            return True
+            return rect_1
