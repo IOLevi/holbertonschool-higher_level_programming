53bc72d08b682cb8647447168e4e408aba7e6cc5
IOLevi <337@holbertonschool.com>
2018-05-17 09:43:57 -0700

    rewrite logic for passing in 0

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,5 +5,13 @@
 def read_lines(filename="", nb_lines=0):
     'reads nb_lines number of lines from a file and prints them'
     with open(filename, encoding="utf-8") as a:
-        for i in range(nb_lines):
-            print(a.readline(), end="")
+        if nb_lines == 0:
+            for i in a:
+                print(i, end="")
+            return
+        count = 0
+        for i in a:
+            print(i, end="")
+            count = count + 1
+            if count == nb_lines:
+                return
