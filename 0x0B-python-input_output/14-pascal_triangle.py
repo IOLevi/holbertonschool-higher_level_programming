cf53c6b2a4f651fb8fc741a66853dbd56807a876
IOLevi <337@holbertonschool.com>
2018-05-17 10:56:31 -0700

    forgot to upload!

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -3,6 +3,16 @@
 
 
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    pt = []
+    'creates a pascal triangle'
+
+    pt = [[1 for i in range(row + 1)] for row in range(n)]
+
+    for i in range(n):
+        for a in range(i):
+            if a == 0 or i == a:
+                pt[i][a] = 1
+            else:
+                pt[i][a] = pt[i-1][a-1] + pt[i-1][a]
+
+    for i in pt:
+        print(i)
