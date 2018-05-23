68e147a832e3636f051726ef741795c65e50627e
IOLevi <337@holbertonschool.com>
2018-05-22 20:04:19 -0700

    more logic

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         "returns the json representation of a list of dictionaries"
-        if list_dictionaries is None:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
 
