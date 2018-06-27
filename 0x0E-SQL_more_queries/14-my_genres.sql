6063f35f749d6a380e58c998ec6fac8aba54a33e
IOLevi <337@holbertonschool.com>
2018-06-26 17:21:48 -0700

    up 2

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,6 @@
+-- uses the database to list all genres of the show DEXTER
+SELECT tv_genres.name
+FROM tv_genres
+JOIN tv_show_genres
+ON tv_show_genres.genre_id=tv_genres.id
+WHERE tv_show_genres.show_id=8;
