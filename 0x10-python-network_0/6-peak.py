7269ddb550591ac5403771c99abce289cad7a934
IOLevi <337@holbertonschool.com>
2018-07-31 23:33:55 -0700

    first

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,3 +2,11 @@
 'finds a peak in a list of unsorted ints'
 
 def find_peak(list_of_integers):
+    'find peak'
+    """
+    go to mid point ()
+    check if it is peak
+    if yes, return
+    if not, go t 
+    """
+    return max(list_of_integers)
