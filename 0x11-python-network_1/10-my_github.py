c11fda144f5439c3a884edfcd50e118eede1fbf2
IOLevi <337@holbertonschool.com>
2018-08-06 23:14:09 -0700

    second up

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# a Python script that takes your Github credentials 
+# (username and password) 
+# and uses the Github API to display your id
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    name = sys.argv[1]
+    pword = sys.argv[2]
+    r = requests.get('https://api.github.com/users/{}'.format(name), auth=requests.auth.HTTPBasicAuth(name, pword))
+
+    
+    try:
+        j = r.json()
+        print(j.get('id'))
+    except ValueError:
+        pass
