c985c8c5a3b8d3ca40f9eb41ee17faa451fdb8c6
IOLevi <337@holbertonschool.com>
2018-08-06 23:47:02 -0700

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
         r = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(r)))
-        print('\t - content: {}'.format(r))
-        print('\t - utf8 content: {}'.format(r.decode('utf-8')))
+        print('\t- content: {}'.format(r))
+        print('\t- utf8 content: {}'.format(r.decode('utf-8')))
