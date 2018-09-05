3ea877db8028b4a46e849c70eaa765f413f6915f
IOLevi <337@holbertonschool.com>
2018-09-05 17:46:47 +0000

    account for error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,17 +4,18 @@ let url = process.argv[2];
 let request = require('request');
 
 request(url, (error, response, body) => {
-  let films = JSON.parse(body)['results'];
-  let counter = 0;
   if (error) {
     console.log(error);
-  }
-  for (let film of films) {
-    for (let guy of film['characters']) {
-      if (guy === 'https://swapi.co/api/people/18/') {
-        counter += 1;
+  } else {
+    let films = JSON.parse(body)['results'];
+    let counter = 0;
+    for (let film of films) {
+      for (let guy of film['characters']) {
+        if (guy === 'https://swapi.co/api/people/18/') {
+          counter += 1;
+        }
       }
     }
+    console.log(counter);
   }
-  console.log(counter);
 });
